16468a7a04d4e7a9b14f724358d0590a8ba60c49
Talha3211 <talha.latif345@gmail.com>
2024-07-23 16:14:12 -0700

    updated ct

--- app/_components/Cabin.js
+++ app/_components/Cabin.js
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import TextExpander from "@/app/_components/TextExpander";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
