d4b3126c5828c2453d5cb49c84eddeb4e2ca8890
Talha3211 <talha.latif345@gmail.com>
2024-07-24 15:30:13 -0700

    updated ctttt

--- app/cabin/[CabinId]/page.js
+++ app/cabin/[CabinId]/page.js
@@ -1,3 +1,4 @@
+
 import Cabin from "@/app/_components/Cabin";
 import Reservation from "@/app/_components/Reservation";
 import Spinner from "@/app/_components/Spinner";
@@ -6,21 +7,21 @@ import { Suspense } from "react";
 
 // export const revalidate = 5;
 
-export async function generateMetadata({params}){
-    const {name} = await  getCabin(params.cabinId)
-    return {title: `Cabin ${name}`}
-}
+// export async function generateMetadata({params}){
+//     const {name} = await  getCabin(params.cabinId)
+//     return {title: `Cabin ${name}`}
+// }
 
-export async function generateStaticParams(){
+// export async function generateStaticParams(){
 
-    const cabins = await getCabins()
+//     const cabins = await getCabins()
    
 
-// console.log(cabins)
-    const ids = cabins.map((cabin)=>({cabinId: String(cabin.id)}))
-    // console.log(ids)
-  return ids
-}
+// // console.log(cabins)
+//     const ids = cabins.map((cabin)=>({cabinId: String(cabin.id)}))
+//     // console.log(ids)
+//   return ids
+// }
 
 
 export default async function Page({params}) {
