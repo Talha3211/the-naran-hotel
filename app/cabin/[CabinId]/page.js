0ac6fe18ff86d424c60949c7237fc82256bfeb61
Talha3211 <talha.latif345@gmail.com>
2024-07-25 14:26:46 -0700

    updated ctyt

--- app/cabin/[CabinId]/page.js
+++ app/cabin/[CabinId]/page.js
@@ -1,42 +1,30 @@
-
 import Cabin from "@/app/_components/Cabin";
 import Reservation from "@/app/_components/Reservation";
 import Spinner from "@/app/_components/Spinner";
-import { getCabin, getCabins, getSettings } from "@/app/_lib/data-service";
+import { getCabin } from "/app/_lib/data-service";
+
+import Image from "next/image";
 import { Suspense } from "react";
 
-// export const revalidate = 5;
+// export const metadata = {
+//   title: "Cabin",
+// };
 
-// export async function generateMetadata({params}){
-//     const {name} = await  getCabin(params.cabinId)
-//     return {title: `Cabin ${name}`}
+// export async function generateMetadata({ params }) {
+//   const { name } = await getCabin(params.cabinId);
+//   return { title: `Cabin ${name}` };
 // }
 
-// export async function generateStaticParams(){
+// export async function generateStaticParams() {
+//   const cabins = await getCabins();
 
-//     const cabins = await getCabins()
-   
+//   const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
 
-// // console.log(cabins)
-//     const ids = cabins.map((cabin)=>({cabinId: String(cabin.id)}))
-//     // console.log(ids)
-//   return ids
+//   return ids;
 // }
 
-
-export default async function Page(props) {
-  console.log(props)
-
-  const { params } = props;
-
-    const cabin = await getCabin(params.cabinId)
-   
-
-    console.log(cabin)
-    // console.log(params)
-//   const { id, name, maxCapacity, regularPrice, discount, image, description } =
-//     cabin;
-// console.log(description)
+export default async function Page({ params }) {
+  const cabin = await getCabin(params.cabinId);
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
@@ -44,7 +32,7 @@ export default async function Page(props) {
 
       <div>
         <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
-          {/* Reserve {cabin.name} today. Pay on arrival. */}
+          Reserve {cabin.name} today. Pay on arrival.
         </h2>
 
         <Suspense fallback={<Spinner />}>
@@ -52,5 +40,5 @@ export default async function Page(props) {
         </Suspense>
       </div>
     </div>
-  )
+  );
 }
