6c4f2c1eb60233e58ec7bdc0cba51a804291503f
Talha3211 <talha.latif345@gmail.com>
2024-07-25 14:37:01 -0700

    updated ctyit

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from 'date-fns';
 import { supabase } from './supabase';
+import { notFound } from 'next/navigation';
 
 
 /////////////
@@ -15,8 +16,10 @@ export async function getCabin(id) {
   // For testing
   // await new Promise((res) => setTimeout(res, 1000));
 
+  console.log(data)
   if (error) {
     console.error(error);
+    notFound()
   }
 
   return data;
