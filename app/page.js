42b3f3c496de56e3bc410925add169b4d0114d53
Talha3211 <talha.laif345@gmail.com>
2024-10-17 11:02:47 +0500

    finall

--- app/page.js
+++ app/page.js
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import bg from '@/public/bg.png'
-
+import bg from "@/public/bg.png";
 
 export default function Page() {
   return (
     <main className="mt-24">
       <Image
-      fill 
-      src={bg}
-      placeholder="blur"
-      className="object-cover object-top"
-      alt="Mountains and forests with two cabins" />
+        fill
+        src={bg}
+        placeholder="blur"
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
 
       <div className="relative z-10 text-center">
-        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+        <h1 className="text-7xl text-primary-50 mb-10 tracking-tight font-normal">
           Welcome to paradise.
         </h1>
         <Link
