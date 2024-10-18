b1b74e2debce334d7a15f390c1f2941fa2626a0b
Talha3211 <talha.laif345@gmail.com>
2024-10-18 11:55:03 +0500

    final

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -56,4 +56,58 @@ export default function UpdateProfileForm({ children, guest }) {
             name="email"
             disabled
             defaultValue={guest.email}
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+          />
+        </div>
+
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <label htmlFor="nationality">Where are you from?</label>
+            <img
+              name="nationality"
+              src={guest.countryFlag}
+              alt="Country flag"
+              className="h-5 rounded-sm"
+            />
+          </div>
+
+          {children}
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="nationalID">National ID number</label>
+          <input
+            defaultValue={guest.nationalID}
+            name="nationalID"
+            onInput={handleNationalIDChange} // Handle input change here
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+          />
+          {idError && (
+            <p className="text-red-500 text-sm">{idError}</p> // Display error message if any
+          )}
+        </div>
+
+        <div className="flex justify-end items-center gap-6">
+          <UpdateButton />
+        </div>
+
+        {formError && (
+          <p className="text-red-500 text-sm mt-4">{formError}</p> // Display general form error
+        )}
+      </form>
+    </div>
+  );
+}
+
+function UpdateButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating profile" : "Update profile"}
+    </button>
+  );
+}
